a027084db08f9b4e0680fa69455382384b207b56
Bittah-C0ding <adrianjhndez@gmail.com>
2026-05-14 09:17:28 -0600

    Add responsive gallery with modal

---
+++ ponder-responsive-gallery-modals/java.js
@@ -0,0 +1,31 @@
+// Made by Adrian Hernandez
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+function openModal(e) {
+    let target = e.target;
+    // Find the actual image element inside the clicked figure
+    if (target.tagName !== 'IMG') {
+        target = target.querySelector('img');
+        if (!target) return;
+    }
+    const smallSrc = target.getAttribute('src');
+    // Replace '-sm.jpg' with '-full.jpg' to get the high-res version
+    const largeSrc = smallSrc.replace('-sm.jpg', '-full.jpg');
+    modalImage.setAttribute('src', largeSrc);
+    modal.showModal();
+}
+
+gallery.addEventListener('click', openModal);
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
